4b5ddcc5b76cde39e4cd5ce4f26448839f167750
inshad <innuinshad2004@gmail.com>
2024-09-19 20:11:39 +0530

    overall Finish

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -3,101 +3,122 @@ import upload_area from "../assets/upload_area.svg";
 import { FaPlus } from "react-icons/fa6";
 import axios from "axios";
 import { toast } from "react-toastify";
+
 const Add = () => {
-	const url = 'http://localhost:4000'
-	const [image, setImage] = useState(false);
+	const url = 'http://localhost:4000';
+	const [image, setImage] = useState(null); // Initialize as null for image
 	const [data, setData] = useState({
-		name:"",
-		description:"",
-		category:"",
-		price:"Women"
+		name: "",
+		description: "",
+		category: "Women", // Default category
+		price: "" // Empty price by default
 	});
 
 	const onChangeHandler = (e) => {
 		const name = e.target.name;
 		const value = e.target.value;
-		setData((data)=>({...data,[name]:value}))
-	}
+		setData((data) => ({ ...data, [name]: value }));
+	};
 
-	const onSubmiHandler = async(event) => {
-		event.preventDefault()
-		const formatData = new FormData();
-		formatData.append('name',data.name);
-		formatData.append('description',data.description);
-		formatData.append('price',Number(data.price))
-		formatData.append('category',data.category)
-		formatData.append('image',image);
-
-		const response = await axios.post(`${url}/api/product/add`,formatData)
-
-		if(response.data.success) {
-			setData({
-				name:'',
-				description:'',
-				price:'',
-				category:''
-			})
-			setImage(false);
-			toast.success(response.data.message);
+	const onSubmitHandler = async (event) => {
+		event.preventDefault();
+		// Ensure image is selected before submitting
+		if (!image) {
+			toast.error("Please upload an image.");
+			return;
 		}
-	}
-    return (
-        <section className="p-4 sm:p-10 w-full bg-primary/20">
-            <form onSubmit={onSubmiHandler} className="flex flex-col gap-y-5 max-w-[555px]">
-                <h4 className="bold-24 pb-2 uppercase">Product Upload</h4>
-                <div className="flex flex-col gap-y-5 max-w-24 h-24">
-                    <p>Upload Image</p>
-                    <label htmlFor="image" >
-                        <img src={image?URL.createObjectURL(image):upload_area} alt="" className="h-20"/>
-                    </label>
-                    <input onChange={(e)=> setImage(e.target.files[0])} type="file" id="image" hidden required />
-                </div>
-                <div className="flex flex-col gap-y-2">
-                    <p>Product Name</p>
-                    <input
-					onChange={onChangeHandler}
-					value={data.name}
-					className="ring-1 ring-slate-900/10 py-1 px-3 outline-none"
-                        type="text"
-                        name="name"
-                        placeholder="Type here....."
-                        id=""
-                    />
-                </div>
-                <div className="flex flex-col gap-y-2">
-                    <p>Product Description</p>
-                    <textarea
-					onChange={onChangeHandler}
-					value={data.description}
-					    className="ring-1 ring-slate-900/10 py-1 px-3 outline-none"
-                        name="description"
-                        placeholder="Add Description"
-                        rows={6}
-                        required
-                    ></textarea>
-                </div>
-                <div className="flex items-center gap-x-6 text-gray-900/70 medium-15">
-                    <div className="flex flex-col gap-y-2">
-                        <p>Product Category</p>
-                        <select name="category" className="outline-none ring-1 ring-slate-900/10 pl-2" onChange={onChangeHandler} value={data.category}>
-                            <option value="Women">Women</option>
-                            <option value="Men">Men</option>
-                            <option value="Kids">Kids</option>
-                            <option value="Unisex">Unisex</option>
-                        </select>
-                    </div>
-                    <div className="flex flex-col gap-y-2">
-                        <p>Product Price</p>
-                        <input className="ring-1 ring-slate-900/10 pl-2 w-24 outline-none" type="number" name="price" placeholder="$20" onChange={onChangeHandler} value={data.price} />
-                    </div>
-                </div>
+		const formatData = new FormData();
+		formatData.append('name', data.name);
+		formatData.append('description', data.description);
+		formatData.append('price', Number(data.price));
+		formatData.append('category', data.category);
+		formatData.append('image', image);
+
+		try {
+			const response = await axios.post(`${url}/api/product/add`, formatData);
+			if (response.data.success) {
+				// Reset form
+				setData({
+					name: '',
+					description: '',
+					price: '',
+					category: 'Women'
+				});
+				setImage(null); // Reset image
+				toast.success(response.data.message);
+			} else {
+				toast.error("Failed to add product.");
+			}
+		} catch (error) {
+			console.error(error);
+			toast.error("An error occurred while adding the product.");
+		}
+	};
+
+	return (
+		<section className="p-4 sm:p-10 w-full bg-primary/20">
+			<form onSubmit={onSubmitHandler} className="flex flex-col gap-y-5 max-w-[555px]">
+				<h4 className="bold-24 pb-2 uppercase">Product Upload</h4>
+				<div className="flex flex-col gap-y-5 max-w-24 h-24">
+					<p>Upload Image</p>
+					<label htmlFor="image">
+						<img src={image ? URL.createObjectURL(image) : upload_area} alt="" className="h-20" />
+					</label>
+					<input onChange={(e) => setImage(e.target.files[0])} type="file" id="image" hidden />
+				</div>
+				<div className="flex flex-col gap-y-2">
+					<p>Product Name</p>
+					<input
+						onChange={onChangeHandler}
+						value={data.name}
+						className="ring-1 ring-slate-900/10 py-1 px-3 outline-none"
+						type="text"
+						name="name"
+						placeholder="Type here..."
+					/>
+				</div>
+				<div className="flex flex-col gap-y-2">
+					<p>Product Description</p>
+					<textarea
+						onChange={onChangeHandler}
+						value={data.description}
+						className="ring-1 ring-slate-900/10 py-1 px-3 outline-none"
+						name="description"
+						placeholder="Add Description"
+						rows={6}
+						required
+					></textarea>
+				</div>
+				<div className="flex items-center gap-x-6 text-gray-900/70 medium-15">
+					<div className="flex flex-col gap-y-2">
+						<p>Product Category</p>
+						<select name="category" className="outline-none ring-1 ring-slate-900/10 pl-2" onChange={onChangeHandler} value={data.category}>
+							<option value="Women">Women</option>
+							<option value="Men">Men</option>
+							<option value="Kids">Kids</option>
+							<option value="Unisex">Unisex</option>
+						</select>
+					</div>
+					<div className="flex flex-col gap-y-2">
+						<p>Product Price</p>
+						<input
+							className="ring-1 ring-slate-900/10 pl-2 w-24 outline-none"
+							type="number"
+							name="price"
+							placeholder="$20"
+							onChange={onChangeHandler}
+							value={data.price}
+							min="0"
+						/>
+					</div>
+				</div>
 				<button type="submit" className="btn-dark sm:w-5/12 flexCenter gap-x-2 !py-2 rounded">
 					<FaPlus />
 					Add Product
 				</button>
-            </form>
-        </section>
-    );
+			</form>
+		</section>
+	);
 };
 
 export default Add;
--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,11 +1,88 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { TbTrash } from 'react-icons/tb';
 
 const List = () => {
-  return (
-	<div>
-	  
-	</div>
-  )
-}
+  const url = 'http://localhost:4000';
+  const [list, setList] = useState([]);
 
-export default List
+  // Fetch product list
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(`${url}/api/product/list`);
+      if (response.data.success) {
+        setList(response.data.data);
+      } else {
+        toast.error('Error fetching products');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      toast.error('An error occurred while fetching the product list.');
+    }
+  };
+
+  // Delete product
+  const deleteProduct = async (id) => {
+    try {
+      const response = await axios.post(`${url}/api/product/remove`, { id });
+      if (response.data.success) {
+        toast.success('Product removed successfully');
+        setList((prevList) => prevList.filter((product) => product._id !== id));
+      } else {
+        toast.error('Failed to remove product');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      toast.error('An error occurred while deleting the product.');
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  return (
+    <section className='p-4 sm:p-10 box-border w-full'>
+      <h4 className="bold-22 uppercase">Product List</h4>
+      <div className='overflow-auto mt-5'>
+        <table className='w-full'>
+          <thead>
+            <tr className='border-b border-slate-900/20 text-gray-30  regular-14 xs:regular-16 text-start py-12'>
+              <th className="p-1 text-left">Product</th>
+              <th className="p-1 text-left">Title</th>
+              <th className="p-1 text-left">Price</th>
+              <th className="p-1 text-left">Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            {list.map((product) => (
+              <tr key={product._id} className='border-b border-slate-900/20 text-gray-50 p-6 medium-14 text-left'>
+                <td className='p-1'>
+                  <img
+				  className='rounded'
+                    src={`${url}/uploads/${product.image}`}
+                    alt={product.name}
+                    height={38}
+                    width={38}
+                  />
+                </td>
+                <td className='p-1'>
+                  <div className='line-clamp-3'>{product.name}</div>
+                </td>
+                <td  className='p-1'>${product.price}</td>
+                <td>
+                  <div onClick={() => deleteProduct(product._id)} className="bold-22 cursor-pointer" >
+                    <TbTrash />
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+};
+
+export default List;
